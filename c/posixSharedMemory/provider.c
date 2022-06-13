c46dea322cda0776ddce51423cb20376e7825681
dimonwang <dimonwang@qq.com>
2022-06-13 00:39:12 -0700

    c [add]: posixSharedMemory

---
+++ c/posixSharedMemory/provider.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <unistd.h>
+
+#define SHM_PATH "/var_tmp_dimon_shm4"
+
+int shm_id = 0; 
+
+int main(void) {
+	printf("posix shared memory provider start.\n");
+	shm_id = shm_open(SHM_PATH, O_RDWR | O_CREAT | O_EXCL, /*O_RDWR*/0777); 
+	if (shm_id < 0) {
+		printf("shm_open:%s\n", strerror(errno));
+		return 1;
+	}
+
+	printf("shm_id:%d\n", shm_id);
+
+	ftruncate(shm_id, sizeof(char));
+
+	char *ptr = mmap(NULL, sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED, shm_id, 0);
+	if (ptr == NULL) {
+		printf("mmap failed:%s\n", strerror(errno));
+		shm_unlink(SHM_PATH);
+		return 2;
+	}
+
+
+	printf("preset content:%d\n", *ptr);
+
+	getchar();
+
+	printf("result content:%d\n", *ptr);
+
+	shm_unlink(SHM_PATH);
+	munmap(ptr, sizeof(char));
+
+	return 0;
+}
---
+++ c/posixSharedMemory/readme.txt
@@ -0,0 +1,3 @@
+1, run provider firstly.
+2, run consumer to modify the shm.
+3, input enter to provider to receive the modified result.
