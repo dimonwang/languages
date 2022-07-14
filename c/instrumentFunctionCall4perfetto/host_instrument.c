94bc772b5af3fe3d2294b3c1951aa7ed930c2978
dimonwang <dimonwang@qq.com>
2022-07-13 23:23:22 -0700

    c [modify]: host instrument

---
+++ c/instrumentFunctionCall4perfetto/host_instrument.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include "instrument.h"
+
+#define INSTRUMENT_FILE_PATH "instrument.log"
+
+static int use_file = 1;
+static FILE *instrument_fd = NULL;
+static int _flag = 0;
+
+#define open_instrument_file()                          \
+    (instrument_fd = fopen(INSTRUMENT_FILE_PATH, "w"))
+
+#define close_instrument_file()                         \
+do {                                                    \
+    if (NULL != instrument_fd)                          \
+    {                                                   \
+        fclose(instrument_fd);                          \
+        instrument_fd = NULL;                           \
+    }                                                   \
+} while(0)
+
+
+#define instrument_print(args, fmt...)                  \
+do {                                                    \
+    if (0 == _flag)                                     \
+    {                                                   \
+        break;                                          \
+    }                                                   \
+    if (use_file != 0 && NULL == instrument_fd && NULL == open_instrument_file())  \
+    {                                                   \
+        printf("Err: Can not open output file.\n");     \
+        break;                                          \
+    }                                                   \
+    if (use_file == 0) {                                \
+        printf(args, ##fmt);                            \
+        break;                                          \
+    }                                                   \
+    fprintf(instrument_fd, args, ##fmt);                \
+    fflush(instrument_fd);                              \
+} while(0)
+
+void  __attribute__((no_instrument_function)) enable_instrument( void )
+{
+    _flag = 1;
+}
+void  __attribute__((no_instrument_function)) disable_instrument( void )
+{
+    _flag = 0;
+}
+
+void  __attribute__((no_instrument_function, destructor)) main_destructor( void )
+{
+    close_instrument_file();
+}
+
+
+void  __attribute__((no_instrument_function)) __cyg_profile_func_enter( void *func_addr, void *call_site )
+{
+    // instrument_print("Enter, call_site:%x\n func_addr:%x\n", call_site, func_addr);
+#define MAX_LEN 17
+    if (_flag == 0) return;
+    char virtualFuncAddr[MAX_LEN];
+    memset(virtualFuncAddr, 0, MAX_LEN);
+    snprintf(virtualFuncAddr, MAX_LEN-1, "%x", func_addr);
+
+    char virtualCallerAddr[MAX_LEN];
+    memset(virtualCallerAddr, 0, MAX_LEN);
+    snprintf(virtualCallerAddr, MAX_LEN-1, "%x", call_site);
+
+    printf("%s:%x, enter %x, caller %x\n", __FUNCTION__, (void*) __cyg_profile_func_enter,
+            (void*)&virtualFuncAddr, (void*)&virtualCallerAddr);
+#undef MAX_LEN
+}
+
+void  __attribute__((no_instrument_function)) __cyg_profile_func_exit( void *func_addr, void *call_site )
+{
+    // instrument_print("Exit, call_site:%x\n func_addr:%x\n", call_site, func_addr);
+#define MAX_LEN 17
+    if (_flag == 0) return;
+    char virtualFuncAddr[MAX_LEN];
+    memset(virtualFuncAddr, 0, MAX_LEN);
+    snprintf(virtualFuncAddr, MAX_LEN-1, "%x", func_addr);
+
+    char virtualCallerAddr[MAX_LEN];
+    memset(virtualCallerAddr, 0, MAX_LEN);
+    snprintf(virtualCallerAddr, MAX_LEN-1, "%x", call_site);
+    printf("%s:%x, exit %x, caller %x\n", __FUNCTION__, (void*) __cyg_profile_func_exit, 
+            (void*)&virtualFuncAddr, (void*)&virtualCallerAddr);
+#undef MAX_LEN
+}
---
+++ c/instrumentFunctionCall4perfetto/host_test_instrument.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "instrument.h"
+#include <unistd.h>
+
+int mul(int i, int j) {
+    printf("%s:%x\n", __FUNCTION__, (void*) mul);
+    return i*j;
+}
+
+int addmul(int i, int j) {
+    printf("%s\n", __FUNCTION__);
+    int k1,k2;
+    k1 = i+j;
+    enable_instrument();
+    k2=mul(i,j);
+    disable_instrument();
+    return k1+k2;
+}
+
+void d123456789flg(){}
+
+int main(void) {
+    printf("%s\n", __FUNCTION__);
+    printf("main:  %x\n", (void*) main);
+    printf("addmul:%x\n", (void*) addmul);
+    printf("mul:   %x\n", (void*) mul);
+    /* enable_instrument(); */
+    int round = 0;
+    while(round++ < 500000) {
+        int i = addmul(3,5);
+        printf("round:%d,result:%d\n", round, i);
+        sleep(1);
+    }
+    /* disable_instrument(); */
+
+    return 0;
+}
