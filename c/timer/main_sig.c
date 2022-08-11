4ad3f98764464d1ff846a0f53637a8d96a78a675
dimonwang <dimonwang@qq.com>
2022-08-11 02:11:42 -0700

    c [add]: timer(both thread and signal modes)

---
+++ c/timer/main_sig.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+
+void timerHandle(int signum) {
+    printf("%s\n", __FUNCTION__);
+}
+
+void timerInit(int sec, int usec)
+{
+    timer_t timerid;
+    struct sigevent evp;
+    memset(&evp, 0, sizeof(struct sigevent)); 
+    evp.sigev_value.sival_int = 111;
+    evp.sigev_notify = SIGEV_SIGNAL;
+    evp.sigev_signo = SIGALRM;
+    /* evp.sigev_notify_function = timerHandle; */
+ 
+    if (timer_create(CLOCK_REALTIME, &evp, &timerid) == -1)
+    {
+        perror("fail to timer_create");
+        exit(-1);
+    }
+ 
+    signal(SIGALRM, timerHandle);
+
+    struct itimerspec it;
+    it.it_interval.tv_sec = sec;
+    it.it_interval.tv_nsec = usec*1000;
+    it.it_value.tv_sec = sec;
+    it.it_value.tv_nsec = usec*1000;
+    if (timer_settime(timerid, 0, &it, NULL) == -1)
+    {
+        perror("fail to timer_settime");
+        exit(-1);
+    }
+}
+
+int main(void) {
+    timerInit(1,0);
+    while(1) {
+        sleep(100);
+    }
+
+	return 0;
+}
