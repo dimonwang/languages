4e684864cae519b0c474fbcf95b3b92a2b50e93b
dimonwang <dimonwang@qq.com>
2022-10-07 20:30:03 -0700

    cxx [add]: so dependencies

---
+++ cxx/so/b.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "sub.hh"
+
+int test_b()
+{
+    return fake_init();
+}
+
---
+++ cxx/so/main.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "a.hh"
+#include "b.hh"
+
+int main() {
+    printf("main a\n");
+    test_a();
+    printf("main b\n");
+    test_b();
+	return 0;
+}
