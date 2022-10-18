5575d5b1fbe996a6060f698f32285bd7d91fbd85
dimonwang <dimonwang@qq.com>
2022-10-18 04:03:15 -0700

    cxx [add]: unused parameters for printf

--- c/printf/main.c
+++ c/printf/main.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <limits.h>
+#include<stdio.h>
 
 int main(void) {
-    long long  a = 0x812345678;//LLONG_MAX;
-	printf("p:%p\n", a);
-	printf("x:%#lx\n", a);
+    void * p = NULL;
+	printf("hello world! from dimon\n", p);
+    void *p2 = 0x233;
+	printf("hello world! from dimon\n", p2);
 	return 0;
 }
