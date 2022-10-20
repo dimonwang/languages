b8c94b04c71f5a0275df98b776548c063cc7030b
dimonwang <dimonwang@qq.com>
2022-10-20 01:41:41 -0700

    cxx [add]: uninitialized data member

--- cxx/uninitializedmember/main.cc
+++ cxx/uninitializedmember/main.cc
@@ -1,20 +1,39 @@
 #include <iostream>
 
+class Mem
+{
+    public:
+        Mem():b(0) {
+            std::cout << "in Mem" << std::endl;
+        };
+        int get() {
+            return b;
+        };
+    private:
+        int b;
+};
+
 class Father
 {
 public:
     Father(){};
 	void show() {
-		std::cout << "in Father:" << a << std::endl;
+		std::cout << "in Father:" << a << ", mem:" << m.get() << std::endl;
 	};
 
 private:
     int a;
+    Mem m;
 };
+
+Father gf;
 
 int main() {
     Father f;
+    std::cout << "--stack--" << std::endl;
     f.show();
 
+    std::cout << "--global--" << std::endl;
+    gf.show();
 	return 0;
 }
