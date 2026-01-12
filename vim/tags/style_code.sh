6f34cbed57065904a1ce28f3c4e37efaff954ddd
dimonwang <dimonwang@qq.com>
2026-01-12 14:57:15 +0800

    Update style_code.sh

--- vim/tags/style_code.sh
+++ vim/tags/style_code.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 source_dir=`find ./ -maxdepth 1 -type d | egrep -v "svn|svrconfig|common|res|proto|^\./$"`
 for dir in ${source_dir}
 do
-    find ${dir} -regex '.*\.\(h\|c\|cpp\|hpp\)' -type f | xargs -n 1 ./astyle --style=allman --indent-classes --indent-switches --indent-namespaces --indent-preproc-block --indent-preproc-define --indent-col1-comments --pad-oper --pad-header --unpad-paren --align-pointer=type --keep-one-line-statements --suffix=none 
-    find $(dir) -regex '.*\.\(h\|c\|cpp\|hpp\)' -type f | xargs dos2unix
+    find ${dir} -regex '.*\.\(h\|c\|cpp\|hpp\)$' -type f | xargs -n 1 ./astyle --style=allman --indent-classes --indent-switches --indent-namespaces --indent-preproc-block --indent-preproc-define --indent-col1-comments --pad-oper --pad-header --unpad-paren --align-pointer=type --keep-one-line-statements --suffix=none 
+    find ${dir} -regex '.*\.\(h\|c\|cpp\|hpp\)$' -type f | xargs dos2unix
 done
